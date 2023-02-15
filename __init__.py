d05d6ac38a784dfc395e8920859d4749fdc490bf
erik.berglund@gmail.com <erik.berglund@gmail.com>
2023-02-15 17:54:15 +0100

    Added tools to print items in the order they appear in most stores. Added some more dialog.

--- __init__.py
+++ __init__.py
@@ -1,16 +1,23 @@
 from mycroft import MycroftSkill, intent_file_handler
 from .database import Database
+from escpos.connections import getNetworkPrinter
+from .category_sorter import sort_items
 
+LIST_NAME = "shopping"
 
 class SimpleShoppingList(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.db = Database()
 
+    def _ensure_list_exists(self):
+        """If the list does not exits, create it."""
+        if not self.db.list_exists(LIST_NAME):
+            self.db.add_list(LIST_NAME)
+
     @intent_file_handler('add.intent')
     def handle_add(self, message):
-        if not self.db.list_exists("shopping"):
-            self.db.add_list("shopping")
+        self._ensure_list_exists()
         item = message.data.get("item")
         data = {"item":item}
         if item:
@@ -23,18 +30,48 @@ class SimpleShoppingList(MycroftSkill):
     @intent_file_handler('read.intent')
     def handle_read(self, message):
         data = {}
-        if not self.db.list_exists("shopping"):
-            self.db.add_list("shopping")
+        self._ensure_list_exists()
         if self.db.list_empty("shopping"):
             self.speak_dialog('list_is_empty')
         else:
             data['items'] = self.string(self.db.read_items("shopping"))
             self.speak_dialog('read_items', data)
 
+    @intent_file_handler('count.intent')
+    def handle_count(self, message):
+        data = {}
+        self._ensure_list_exists()
+        if self.db.list_empty("shopping"):
+            self.speak_dialog('list_is_empty')
+        else:
+            count = len(self.db.read_items("shopping"))
+            if count == 1:
+                data['count'] = f"is {count} item"
+                data['short_count'] = f"{count} item"
+            else:
+                data['count'] = f"are {count} items"
+                data['short_count'] = f"{count} items"
+            self.speak_dialog('count', data)
+
+    @intent_file_handler('print.intent')
+    def handle_print(self, message):
+        self._ensure_list_exists()
+        if self.db.list_empty("shopping"):
+            self.speak_dialog('list_is_empty')
+        else:
+            entries = self.db.read_items("shopping")
+            # Sort the entries according to where they appear in the store
+            entries = sort_items(entries)
+            printer = getNetworkPrinter()(host='printer', port=9100)
+            for entry in entries:
+                printer.text(entry)
+                printer.lf()
+                printer.cr()
+            self.speak_dialog('print_items')
+
     @intent_file_handler('delete_last.intent')
     def handle_delete_last(self, message):
-        if not self.db.list_exists("shopping"):
-            self.db.add_list("shopping")
+        self._ensure_list_exists()
         if self.db.list_empty("shopping"):
             self.speak_dialog('list_is_empty')
         else:
@@ -45,8 +82,7 @@ class SimpleShoppingList(MycroftSkill):
     @intent_file_handler('delete.intent')
     def handle_delete(self, message):
         item = message.data.get("item")
-        if not self.db.list_exists("shopping"):
-            self.db.add_list("shopping")
+        self._ensure_list_exists()
         if self.db.list_empty("shopping"):
             self.speak_dialog('list_is_empty')
         else:
@@ -61,10 +97,29 @@ class SimpleShoppingList(MycroftSkill):
 
     def confirm_deletion(self, element):
         """ Make sure the user really wants to delete 'element' """
-
         resp = self.ask_yesno('confirm.deletion', data={'element': element})
         if resp == 'yes':
             return True
+        else:
+            self.speak_dialog('cancelled')
+        return False
+
+
+    @intent_file_handler('clear_list.intent')
+    def handle_clear_list(self, message):
+        self._ensure_list_exists()
+        if self.db.list_empty("shopping"):
+            self.speak_dialog('list_is_empty')
+        else:
+            self.clear_list()
+
+
+    def clear_list(self):
+        """Clear the shopping list"""
+        resp = self.ask_yesno('confirm.clear')
+        if resp == 'yes':
+            self.db.del_list("shopping")
+            self.speak_dialog('list_is_empty')
         else:
             self.speak_dialog('cancelled')
         return False
---
+++ category_sorter.py
@@ -0,0 +1,82 @@
+from typing import List
+
+import nltk
+from nltk.corpus import wordnet
+
+autocategories = {"cheese": "cheese",
+                  "wipes": "cleaning",
+                  "toilet": "cleaning",
+                  "washing": "cleaning",
+                  "soap": "cleaning",
+                  "spray": "cleaning"}
+
+capture_categories = ["vegetable", "plant_material", "plant", "fruit", "bakery", "baked_goods", "meat", "poultry",
+                      "cheese", "roe", "mexican", "olive oil", "pasta", "sauce", "grain", "salt", "preservative",
+                      "dairy_product", "eggs", "spread", "juice", "container", "pizza", "chips", "ice cream", "fish",
+                      "seafood", "nutriment", "sweetening", "flour", "porridge", "tea", "coffee", "peanut butter",
+                      "cleaning", "cleansing_agent", "toiletry", "snacks", "sweet"]
+
+
+def classify_term(item, capture_categories: [str]) -> str:
+    """Find a match in a string with several words."""
+    items = item.split(" ")
+    for item in items:
+        tmp = do_recursive_classification(item, capture_categories)
+        if tmp:
+            return tmp
+    return None
+
+
+def classify_grocery_item(item, capture_categories: [str], remaining_recursives: int = 8) -> str:
+    """Find the class of the grocery item, or 'misc' if not found."""
+    res = do_recursive_classification(item, capture_categories, remaining_recursives)
+    if not res:
+        res = "misc"
+    return res
+
+
+def do_recursive_classification(item, capture_categories: [str], remaining_recursives: int = 8) -> str:
+    if remaining_recursives <= 0:
+        return None
+    res = None
+    if item in autocategories:
+        res = autocategories[item]
+    if item in capture_categories:
+        res = item
+    else:
+        synsets = wordnet.synsets(item)
+        if len(synsets) > 0:
+            synset = synsets[0]
+            hypernyms = synset.hypernyms()
+            if len(hypernyms) > 0:
+                for hypernym in hypernyms:
+                    tmp = hypernym.name().split('.')[0]
+                    if tmp in capture_categories:
+                        res = tmp
+                        break
+                if not res:
+                    # No suitable hypernym found. Look for hypernyms of hypernyms
+                    for hypernym in hypernyms:
+                        tmp = hypernym.name().split('.')[0]
+                        tmp_class = do_recursive_classification(tmp, capture_categories, remaining_recursives - 1)
+                        if tmp_class:
+                            res = tmp_class
+                            break
+
+    if not res and " " in item:
+        res = classify_term(item, capture_categories)
+    return res
+
+
+def sort_items(items: [str]) -> List[str]:
+    nltk.download('wordnet')
+    the_list = []
+    for item in items:
+        classification = classify_grocery_item(item, capture_categories)
+        if classification in capture_categories:
+            index = capture_categories.index(classification)
+            the_list.append((item, index))
+        else:
+            the_list.append((item, len(capture_categories)))
+    the_list = sorted(the_list, key=lambda x: x[1])
+    return [x[0] for x in the_list]
