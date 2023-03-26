493c21099efd143fcd4895e0844c2773bc1a8023
erikjber <erik.berglund@gmail.com>
2023-03-26 16:54:18 +0200

    Google Keep integration.

--- __init__.py
+++ __init__.py
@@ -3,6 +3,9 @@ from .database import Database
 from escpos.connections import getNetworkPrinter
 from .category_sorter import sort_items
 from Levenshtein import distance
+from os.path import dirname, join
+import gkeepapi
+import json
 
 LIST_NAME = "shopping"
 
@@ -10,6 +13,12 @@ class SimpleShoppingList(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.db = Database()
+        self.keep = gkeepapi.Keep()
+        json_path = join(dirname(__file__),"auth.txt")
+        with open(json_path, 'r') as json_file:
+            auth = json.load(json_file)
+        self.keep.resume(auth["email"],auth["token"])
+        self.sync_keep_list()
 
     def _ensure_list_exists(self):
         """If the list does not exits, create it."""
@@ -19,6 +28,7 @@ class SimpleShoppingList(MycroftSkill):
     @intent_file_handler('add.intent')
     def handle_add(self, message):
         self._ensure_list_exists()
+        self.sync_keep_list()
         item = message.data.get("item")
         data = {"item":item}
         if item:
@@ -26,12 +36,14 @@ class SimpleShoppingList(MycroftSkill):
                 self.speak_dialog('already_exists',data)
             else:
                 self.db.add_item("shopping",item)
+                self.sync_keep_list()
                 self.speak_dialog('add',data)
 
     @intent_file_handler('read.intent')
     def handle_read(self, message):
         data = {}
         self._ensure_list_exists()
+        self.sync_keep_list()
         if self.db.list_empty("shopping"):
             self.speak_dialog('list_is_empty')
         else:
@@ -42,6 +54,7 @@ class SimpleShoppingList(MycroftSkill):
     def handle_count(self, message):
         data = {}
         self._ensure_list_exists()
+        self.sync_keep_list()
         if self.db.list_empty("shopping"):
             self.speak_dialog('list_is_empty')
         else:
@@ -57,6 +70,7 @@ class SimpleShoppingList(MycroftSkill):
     @intent_file_handler('print.intent')
     def handle_print(self, message):
         self._ensure_list_exists()
+        self.sync_keep_list()
         if self.db.list_empty("shopping"):
             self.speak_dialog('list_is_empty')
         else:
@@ -78,6 +92,7 @@ class SimpleShoppingList(MycroftSkill):
         else:
             item = self.db.read_items("shopping")[-1]
             self.delete_item(item)
+            self.sync_delete(item)
 
 
     @intent_file_handler('delete.intent')
@@ -106,13 +121,16 @@ class SimpleShoppingList(MycroftSkill):
                     self.speak_dialog("deleted", data)
             else:
                 self.delete_item(item)
-
+            self.sync_keep_list()
 
 
     def delete_item(self, item: str):
         if self.confirm_deletion(item):
             self.db.del_item("shopping", item)
+            self.sync_delete(item)
             self.speak_dialog('deleted', {"item":item})
+            self.sync_keep_list()
+
 
 
     def confirm_deletion(self, element):
@@ -139,10 +157,57 @@ class SimpleShoppingList(MycroftSkill):
         resp = self.ask_yesno('confirm.clear')
         if resp == 'yes':
             self.db.del_list("shopping")
+            self.keep.sync()
+            shoppinglist = next(self.keep.find(query="shoppinglist", archived=False, trashed=False), None)
+            for item in shoppinglist.items:
+                item.delete()
+            self.keep.sync()
             self.speak_dialog('list_is_empty')
         else:
             self.speak_dialog('cancelled')
         return False
+
+    def find_item(self, list, name: str):
+        """Find an item in the keep list."""
+        for item in list.items:
+            if item.text == name:
+                return item
+        return None
+
+    def sync_delete(self, item: str):
+        self.keep.sync()
+        shoppinglist = next(self.keep.find(query="shoppinglist", archived=False, trashed=False), None)
+        remote_item = self.find_item(shoppinglist,item)
+        remote_item.delete()
+        self.keep.sync()
+
+    def sync_keep_list(self):
+        """Sync the local database and the keep list."""
+        self.keep.sync()
+        shoppinglist = next(self.keep.find(query="shoppinglist", archived=False, trashed=False), None)
+        if not shoppinglist:
+            shoppinglist = self.keep.createList("shoppinglist")
+        # Make sure all items on local list are on keep list
+        entries = self.db.read_items("shopping")
+        if entries:
+            # Sort the entries according to where they appear in the store
+            entries = sort_items(entries)
+            index = len(entries)+1
+            for entry in entries:
+                remote_entry = self.find_item(shoppinglist,entry)
+                if not remote_entry:
+                    remote_entry = shoppinglist.add(entry, False, gkeepapi.node.NewListItemPlacementValue.Bottom)
+                remote_entry.sort = index
+                index -= 1
+        # Delete all items that are checked from both lists
+        checked_items = shoppinglist.checked
+        print(f"Checked items: {checked_items}")
+        for item in checked_items:
+            name = item.text
+            if self.db.item_exists("shopping",name):
+                self.db.del_item("shopping",name)
+            item.delete()
+        self.keep.sync()
 
 
     def string(self, lists):
